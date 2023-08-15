005dfa8fe5502036ca94c9c347809fc85e8b5ded
Lara Rondon <larar_918@hotmail.com>
2023-08-15 10:23:57 +0200

    Ejercicio 1

---
+++ ejercicio1.js
@@ -0,0 +1,17 @@
+/* Crea un archivo ejercicio1.js que tenga un objeto con los siguientes campos: Nombre,
+apellidos, un array con los temas del bootcamp que ya conoces, si estás en busqueda
+activa con un boolean y un array de objetos el cual tenga un link a alguna red social con el
+nombre y link de la red social. (Con uno vale, Github por ejemplo, pero dentro de un array).*/
+
+let usuario = {
+    nombre: 'Lara',
+    apellidos: 'Rondon Ergueta',
+    temas : ['git','html','css','javascript'],
+    busqueda_activa : true,
+    red_social: [
+        {
+            nombre: 'GitHub',
+            url: 'https://github.com/larar918'
+        }
+    ]
+}
